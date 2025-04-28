d7f8446fe37e31d2f1d173473ef5ec4477a6f60e
kumataro <kumataro4160@gmail.com>
2025-04-28 19:36:55 +0900

    update

--- realmath.base.h
+++ realmath.base.h
@@ -2,17 +2,17 @@
 #define KUMATARO_INCLUDE_REALMATH_BASE_H
 
 
-namespace kuma
-{
-
 #ifndef KUMATARO_DEFINE_FLOAT_TYPE
 #define KUMATARO_DEFINE_FLOAT_TYPE
-
+namespace kuma
+{
 	using float64_t = double;
 	using float32_t = float;
-
+}
 #endif
 
+namespace kuma
+{
 	using sign_t = signed char;
 	using whole_t = signed long long;
 }
