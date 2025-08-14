d6938c4c54beb2e91de864778d90149a2efc5599
kumataro <kumataro4160@gmail.com>
2025-08-15 02:59:27 +0900

    update realmath.range.h

--- realmath.range.h
+++ realmath.range.h
@@ -40,11 +40,75 @@ namespace kuma
 		{
 			return u - l;
 		}
-		constexpr T clamp(T x)const noexcept
+		constexpr bool contains(T x)const noexcept
 		{
-			return std::clamp(x, l, u);
+			return l <= x && x <= u;
 		}
+		
+		friend constexpr bool operator==(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr bool operator!=(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr bool operator<(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr bool operator>(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr bool operator<=(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr bool operator>=(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr range<T> operator&(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr range<T> operator|(const range<T>& lhs, const range<T>& rhs)noexcept;
+		friend constexpr T clamp(const T& x, const range<T>& r)noexcept;
 	};
+
+	template <class T>
+	constexpr bool operator==(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return lhs.l == rhs.l && lhs.u == rhs.u;
+	}
+
+	template <class T>
+	constexpr bool operator!=(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return !(lhs == rhs);
+	}
+
+	template <class T>
+	constexpr bool operator<(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return lhs.u < rhs.l;
+	}
+
+	template <class T>
+	constexpr bool operator>(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return rhs < lhs;
+	}
+
+	template <class T>
+	constexpr bool operator<=(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return !(rhs < lhs);
+	}
+
+	template <class T>
+	constexpr bool operator>=(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return !(lhs < rhs);
+	}
+
+	template <class T>
+	constexpr range<T> operator&(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return range<T>(std::max(lhs.lower(), rhs.lower()), std::min(lhs.upper(), rhs.upper()));
+	}
+
+	template <class T>
+	constexpr range<T> operator|(const range<T>& lhs, const range<T>& rhs)noexcept
+	{
+		return range<T>(std::min(lhs.lower(), rhs.lower()), std::max(lhs.upper(), rhs.upper()));
+	}
+
+	template <class T>
+	constexpr T clamp(const T& x, const range<T>& r)noexcept
+	{
+		return std::clamp(x, r.lower(), r.upper());
+	}
 }
 
 
